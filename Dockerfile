0a093b0b582057c28d1560776859404f35575c1c
Sam McCord <samuel.e.mccord@gmail.com>
2016-09-28 10:20:04 -0600

    Optimizations, docker only

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
-FROM node:latest
+FROM iron/base:edge
+
+RUN echo '@edge http://nl.alpinelinux.org/alpine/edge/main' >> /etc/apk/repositories
+# RUN echo '@community http://nl.alpinelinux.org/alpine/edge/community' >> /etc/apk/repositories
+
+RUN apk update && apk upgrade \
+  && apk add nodejs@edge bash g++ krb5-dev curl make python \
+  && rm -rf /var/cache/apk/*
 
 RUN mkdir parse
 
@@ -6,9 +13,10 @@ ADD . /parse
 WORKDIR /parse
 RUN npm install
 
-ENV APP_ID setYourAppId
-ENV MASTER_KEY setYourMasterKey
-ENV DATABASE_URI setMongoDBURI
+# ENV APP_ID setYourAppId
+# ENV MASTER_KEY setYourMasterKey
+# ENV DATABASE_URI setMongoDBURI
+ENV NODE_ENV=production
 
 # Optional (default : 'parse/cloud/main.js')
 # ENV CLOUD_CODE_MAIN cloudCodePath
@@ -21,6 +29,6 @@ EXPOSE 1337
 # Uncomment if you want to access cloud code outside of your container
 # A main.js file must be present, if not Parse will not start
 
-# VOLUME /parse/cloud               
+# VOLUME /parse/cloud
 
 CMD [ "npm", "start" ]
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // compatible API routes.
 
 var express = require('express');
+var compression = require('compression');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
@@ -13,7 +14,6 @@ if (!databaseUri) {
 
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
@@ -26,24 +26,11 @@ var api = new ParseServer({
 // javascriptKey, restAPIKey, dotNetKey, clientKey
 
 var app = express();
-
-// Serve static assets from the /public folder
-app.use('/public', express.static(path.join(__dirname, '/public')));
+app.use(compression());
 
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
-
-// Parse Server plays nicely with the rest of your web routes
-app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
-});
-
-// There will be a test page available on the /test path of your server url
-// Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
-});
 
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
